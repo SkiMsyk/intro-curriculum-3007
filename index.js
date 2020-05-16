aa16e0346e9c524ded0e4bdd64c6f13af885f95a
Masayuki Sakai <Sakai.Masayuki@keio.jp>
2020-05-16 11:05:08 +0900

    complete this exercise

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * return 17x flags
+ * @param {integer}
+ * @return {array} of boolean
+ */
+function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
